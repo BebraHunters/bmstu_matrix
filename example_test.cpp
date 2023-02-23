950d72224b3182558131e4312ce4f5f053363777
Valentin-Igrevsky <valentin.igrevsky@gmail.com>
2023-02-24 02:34:02 +0300

    ADD matrix

--- example_test.cpp
+++ example_test.cpp
@@ -1,10 +1,3 @@
 #include <gtest/gtest.h>
 #include <string>
-TEST(GoogleTestCi, Foo) {
-  ASSERT_TRUE(true);
-}
-TEST(ExampleTest, DmitriiLeonidovich){
-    std::string dml = "DmitriiLeonidovich";
-    const char * dml_c = "DmitriiLeonidovich";
-    ASSERT_STREQ(dml.c_str(), dml_c);
-}
+#include "bmstu_matrix.h"
