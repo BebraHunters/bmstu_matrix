e4d086a22be53a31c297f3d50bdd743f601feff8
plutnik <113545797+plutskiy@users.noreply.github.com>
2023-06-03 01:13:13 +0300

    add try->catch

--- example_test.cpp
+++ example_test.cpp
@@ -2,21 +2,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "bmstu_matrix.h"
+#include "bmstu_matriXXX.h"
 
 TEST(Constructor, Default) {
     bmstu::matrix<int> my_matrix;
-//    ASSERT_EQ(my_matrix.r_count(), 1); Проверка кол-ва строк
-//    ASSERT_EQ(my_matrix.c_count(), 1); Проверка кол-ва столбцов
-//    ASSERT_EQ(my_matrix.size(), 1);   Проверка кол-ва элементов
     ASSERT_EQ(my_matrix(0, 0), 0);
 }
 
 TEST(Constructor, Parameter) {
     bmstu::matrix<int> my_matrix(4, 5);
-//    ASSERT_EQ(my_matrix.r_count(), 4); Проверка кол-ва строк
-//    ASSERT_EQ(my_matrix.c_count(), 5); Проверка кол-ва столбцов
-//    ASSERT_EQ(my_matrix.size(), 20);   Проверка кол-ва элементов
     for (int i = 0; i < 4; ++i) {
         for (int j = 0; j < 5; ++j) {
             ASSERT_EQ(my_matrix(i, j), 0);
@@ -26,16 +20,18 @@ TEST(Constructor, Parameter) {
 
 TEST(Constructor, IList) {
     bmstu::matrix my_matrix({1, 2, 3, 4, 5, 6}, 2, 3);
-//    ASSERT_EQ(my_matrix.r_count(), 2); Проверка кол-ва строк
-//    ASSERT_EQ(my_matrix.c_count(), 3); Проверка кол-ва столбцов
-//    ASSERT_EQ(my_matrix.size(), 6);   Проверка кол-ва элементов
     for (int i = 0; i < 2; ++i) {
         for (int j = 0; j < 3; ++j) {
             ASSERT_EQ(my_matrix(i, j), i * 3 + j + 1);
         }
     }
 
-    ASSERT_THROW(bmstu::matrix bad_matrix({1, 2, 3, 4, 5}, 2, 3), std::out_of_range);
+    try{
+        bmstu::matrix bad_matrix({1, 2, 3, 4, 5}, 2, 3);
+    }
+    catch (const std::out_of_range &err){
+        ASSERT_EQ(err.what(), std::string("Current matrix pos wrong (2, 3) != 5"));
+    }
 }
 
 TEST(Operator, LeftShift) {
@@ -85,7 +81,12 @@ TEST(Method, det) {
     ASSERT_EQ(good_matrix.det(), -240);
 
     bmstu::matrix bad_matrix({1, 2, 3, 4, 5, 6}, 2, 3);
-    ASSERT_THROW(bad_matrix.det(), std::logic_error);
+    try{
+        bad_matrix.det();
+    }
+    catch (const std::logic_error &err) {
+        ASSERT_EQ(err.what(), std::string("Matrix have determinant if (rows == colimns)!"));
+    }
 }
 
 TEST(Operator, Multiplication_1) {
@@ -93,9 +94,6 @@ TEST(Operator, Multiplication_1) {
     bmstu::matrix right_1({1, 2, 3, 4, 5, 6, 7, 8, 9}, 3, 3);
     bmstu::matrix res_1 = left_1 * right_1;
     bmstu::matrix correct_1({41, 49, 57, 22, 29, 36, 79, 98, 117}, 3, 3);
-//    ASSERT_EQ(res_1.r_count(), 3); Проверка кол-ва строк
-//    ASSERT_EQ(res_1.c_count(), 3); Проверка кол-ва столбцов
-//    ASSERT_EQ(res_1.size(), 9);   Проверка кол-ва элементов
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
             ASSERT_EQ(res_1(i, j), correct_1(i, j));
@@ -106,16 +104,18 @@ TEST(Operator, Multiplication_1) {
     bmstu::matrix right_2({1, 2, 3, 1, 10, 4, 5, 6, 4, -5}, 2, 5);
     bmstu::matrix res_2 = left_2 * right_2;
     bmstu::matrix correct_2({13, 17, 21, 13, -5, 8, 13, 18, 8, 35, 37, 50, 63, 37, 10}, 3, 5);
-//    ASSERT_EQ(res_2.r_count(), 3); Проверка кол-ва строк
-//    ASSERT_EQ(res_2.c_count(), 5); Проверка кол-ва столбцов
-//    ASSERT_EQ(res_2.size(), 15);   Проверка кол-ва элементов
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 5; ++j) {
             ASSERT_EQ(res_2(i, j), correct_2(i, j));
         }
     }
 
-    ASSERT_THROW(left_1 * right_2, std::logic_error);
+    try{
+        left_1 * right_2;
+    }
+    catch (const std::logic_error &err) {
+        ASSERT_EQ(err.what(), std::string("The left matrix must have as many columns at the right one has rows!"));
+    }
 }
 
 TEST(Operator, Multiplication_2) {
@@ -134,9 +134,6 @@ TEST(Operator, Concatenation) {
     bmstu::matrix right_1({1, 2, 3, 4, 5, 6, 7, 8, 9}, 3, 3);
     bmstu::matrix res_1 = left_1 + right_1;
     bmstu::matrix correct_1({2, 5, 7, 8, 6, 8, 12, 16, 15}, 3, 3);
-//    ASSERT_EQ(res_1.r_count(), 3); Проверка кол-ва строк
-//    ASSERT_EQ(res_1.c_count(), 3); Проверка кол-ва столбцов
-//    ASSERT_EQ(res_1.size(), 9);   Проверка кол-ва элементов
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
             ASSERT_EQ(res_1(i, j), correct_1(i, j));
@@ -145,7 +142,13 @@ TEST(Operator, Concatenation) {
 
     bmstu::matrix left_2({1, 3, 4, 1, 5, 8}, 3, 2);
     bmstu::matrix right_2({1, 2, 3, 4, 5, 6, 7, 8, 9}, 3, 3);
-    ASSERT_THROW(left_2 + right_2, std::logic_error);
+
+    try{
+        left_2 + right_2;
+    }
+    catch (const std::logic_error &err) {
+        ASSERT_EQ(err.what(), std::string("Matries must have the same dimension!"));
+    }
 }
 
 TEST(Operator, Subtraction) {
@@ -153,9 +156,6 @@ TEST(Operator, Subtraction) {
     bmstu::matrix right_1({1, 2, 3, 4, 5, 6, 7, 8, 9}, 3, 3);
     bmstu::matrix res_1 = left_1 - right_1;
     bmstu::matrix correct_1({0, 1, 1, 0, -4, -4, -2, 0, -3}, 3, 3);
-//    ASSERT_EQ(res_1.r_count(), 3); Проверка кол-ва строк
-//    ASSERT_EQ(res_1.c_count(), 3); Проверка кол-ва столбцов
-//    ASSERT_EQ(res_1.size(), 9);   Проверка кол-ва элементов
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
             ASSERT_EQ(res_1(i, j), correct_1(i, j));
@@ -164,7 +164,14 @@ TEST(Operator, Subtraction) {
 
     bmstu::matrix left_2({1, 3, 4, 1, 5, 8}, 3, 2);
     bmstu::matrix right_2({1, 2, 3, 4, 5, 6, 7, 8, 9}, 3, 3);
-    ASSERT_THROW(left_2 - right_2, std::logic_error);
+
+    try{
+        left_2 - right_2;
+    }
+    catch (const std::logic_error &err) {
+        ASSERT_EQ(err.what(), std::string("Matries must have the same dimension!"));
+    }
+
 }
 
 TEST(Method, get_minor_matrix) {
@@ -172,28 +179,46 @@ TEST(Method, get_minor_matrix) {
     bmstu::matrix minor = my_matrix.get_minor_matrix(1, 1);
     bmstu::matrix correct({1, 3, 5, 4, -5, 7}, 3, 2);
 
-//    ASSERT_EQ(minor.r_count(), 3); Проверка кол-ва строк
-//    ASSERT_EQ(minor.c_count(), 2); Проверка кол-ва столбцов
-//    ASSERT_EQ(minor.size(), 6);   Проверка кол-ва элементов
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 2; ++j) {
             ASSERT_EQ(minor(i, j), correct(i, j));
         }
     }
 
-    ASSERT_THROW(my_matrix.get_minor_matrix(-1, 1), std::out_of_range);
-    ASSERT_THROW(my_matrix.get_minor_matrix(0, -1), std::out_of_range);
-    ASSERT_THROW(my_matrix.get_minor_matrix(4, 1), std::out_of_range);
-    ASSERT_THROW(my_matrix.get_minor_matrix(0, 3), std::out_of_range);
+    try{
+        my_matrix.get_minor_matrix(-1, 1);
+    }
+    catch (const std::out_of_range &err) {
+        ASSERT_EQ(err.what(), std::string("Wrong number of rows or columns!"));
+    }
+
+    try{
+        my_matrix.get_minor_matrix(0, -1);
+    }
+    catch (const std::out_of_range &err) {
+        ASSERT_EQ(err.what(), std::string("Wrong number of rows or columns!"));
+    }
+
+    try{
+        my_matrix.get_minor_matrix(4, 1);
+    }
+    catch (const std::out_of_range &err) {
+        ASSERT_EQ(err.what(), std::string("Wrong number of rows or columns!"));
+    }
+
+    try{
+        my_matrix.get_minor_matrix(0, 3);
+    }
+    catch (const std::out_of_range &err) {
+        ASSERT_EQ(err.what(), std::string("Wrong number of rows or columns!"));
+    }
 }
 
 TEST(Method, transpose) {
     bmstu::matrix my_matrix({1, 2, 3, 1, 10, 4, 5, 6, 4, -5, 4, 7}, 4, 3);
     my_matrix.transpose();
     bmstu::matrix correct({1, 1, 5, -5, 2, 10, 6, 4, 3, 4, 4, 7}, 3, 4);
-//    ASSERT_EQ(my_matrix.r_count(), 3); Проверка кол-ва строк
-//    ASSERT_EQ(my_matrix.c_count(), 4); Проверка кол-ва столбцов
-//    ASSERT_EQ(my_matrix.size(), 12);   Проверка кол-ва элементов
+
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 4; ++j) {
             ASSERT_EQ(my_matrix(i, j), correct(i, j));
@@ -212,16 +237,18 @@ TEST(Method, adj) {
     }
 
     bmstu::matrix bad_matrix({1, 2, 3, 1, 10, 4, 5, 6, 4, -5, 4, 7}, 4, 3);
-    ASSERT_THROW(bad_matrix.adj(), std::logic_error);
+    try{
+        bad_matrix.adj();
+    }
+    catch (std::logic_error &err){
+        ASSERT_EQ(err.what(), std::string("Matrix have adjugate matrix if (rows == columns)"));
+    }
 }
 
 TEST(Method, reverse) {
     bmstu::matrix<double> my_matrix({2, 5, 7, 6, 3, 4, 5, -2, -3}, 3, 3);
     bmstu::matrix res = my_matrix.reverse();
     bmstu::matrix correct({1, -1, 1, -38, 41, -34, 27, -29, 24}, 3, 3);
-//    ASSERT_EQ(res.r_count(), 3); Проверка кол-ва строк
-//    ASSERT_EQ(res.c_count(), 3); Проверка кол-ва столбцов
-//    ASSERT_EQ(res.size(), 9);   Проверка кол-ва элементов
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
             ASSERT_EQ(res(i, j), correct(i, j));
@@ -243,8 +270,18 @@ TEST(Method, reverse) {
     }
 
     bmstu::matrix<double> bad_matrix_1({1, 1, 5, -5, 2, 10, 6, 4, 3, 4, 4, 7}, 3, 4);
-    ASSERT_THROW(bad_matrix_1.reverse(), std::logic_error);
+    try{
+        bad_matrix_1.reverse();
+    }
+    catch (const std::logic_error &err) {
+        ASSERT_EQ(err.what(), std::string("The matrix must be square!"));
+    }
 
     bmstu::matrix<double> bad_matrix_2({1, 2, 3, 4, 5, 6, 7, 8, 9}, 3, 3);
-    ASSERT_THROW(bad_matrix_2.reverse(), std::logic_error);
+    try{
+        bad_matrix_2.reverse();
+    }
+    catch (const std::logic_error &err) {
+        ASSERT_EQ(err.what(), std::string("The determinant of the matrix is 0. The reversed matrix does not exist!"));
+    }
 }
